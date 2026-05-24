babd51ff98f451d72804d1c62769fae4828c6069
Arav Jain <240245776+Unstoppable-AJ-LimitsNotFound@users.noreply.github.com>
2026-05-24 09:23:30 +0000

    Add logic for human choice

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+// Add logic for computer choice
+
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 function getComputerChoice() {
-    let choice = getRandomArbitrary(1,4);
+    let computerChoice = getRandomArbitrary(1,4);
     // console.log(choice);
 
-    switch (choice) {
+    switch (computerChoice) {
         case 1: return "Rock";
         break;
         case 2: return "Paper";
@@ -15,5 +17,22 @@ function getComputerChoice() {
     }
 }
 
-let ans = getComputerChoice();
-console.log(ans);
+let ans1 = getComputerChoice();
+console.log(ans1);
+
+
+// Add logic for human choice
+
+function getHumanChoice() {
+    let humanChoice = prompt("What's your choice?");
+
+    switch (humanChoice) {
+        case "Rock": return "Rock";
+        break;
+        case "Paper": return "Paper";
+        break;
+        case "Scissors": return "Scissors";
+    }
+}
+let ans2 = getHumanChoice();
+console.log(ans2);
